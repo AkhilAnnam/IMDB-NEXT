651a081a90dda6a3b3ca557800c650b4f6339a9c
Akhil Patel <akhilpatelannam@gmail.com>
2024-07-04 23:09:32 +0530

    added about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto p-3 gap-4 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Movies have long captivated audiences worldwide, offering a blend of
+        entertainment, storytelling, and cultural reflection. From the silent
+        films of the early 20th century to today's blockbusters with
+        cutting-edge CGI, movies have evolved significantly. They serve not only
+        as a form of escapism but also as a powerful medium for social
+        commentary, addressing issues ranging from politics to human
+        relationships.
+      </p>
+      <p>
+        One of the defining characteristics of movies is their ability to
+        transport viewers to different times, places, and emotions. Whether it's
+        a period drama set in ancient Rome or a futuristic sci-fi adventure,
+        movies create immersive experiences that engage the senses and provoke
+        thought. Through storytelling techniques such as cinematography, sound
+        design, and narrative structure, filmmakers craft narratives that
+        resonate with audiences on a profound level.
+      </p>
+    </div>
+  );
+}
