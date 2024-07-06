99f20b3273f5fde24b7a2f981b5f37289aa46d96
Akhil Patel <akhilpatelannam@gmail.com>
2024-07-06 21:56:11 +0530

    add card component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,17 +1,12 @@
 import React from "react";
-import { BsTypeH4 } from "react-icons/bs";
+
+import Card from "./Card";
 
 export default function Results({ results }) {
-  console.log(results[0]);
-  // console.log(typeof(results))
   return (
-    <div>
+    <div className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w--6xl max-auto py-4">
       {results.map((result) => (
-        <>
-          <div key={result?.id}>
-            <h2>{result?.original_title}</h2>
-          </div>
-        </>
+        <Card key={result.id} result={result} />
       ))}
     </div>
   );
